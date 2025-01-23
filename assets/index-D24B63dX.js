797996c70c67615a7d0c4b12199c2ecc16863832
jylim-dev <jylim-dev@users.noreply.github.com>
2025-01-23 03:43:34 +0000

    deploy: b5a0844af924959858ce6f15ebc2af3e88c1bbca

--- assets/index-D24B63dX.js
+++ assets/index-D24B63dX.js
@@ -7052,6 +7052,70 @@ function requireClient() {
   return client;
 }
 var clientExports = requireClient();
+const scriptRel = "modulepreload";
+const assetsURL = function(dep) {
+  return "/weekly-retrospective/" + dep;
+};
+const seen = {};
+const __vitePreload = function preload(baseModule, deps, importerUrl) {
+  let promise = Promise.resolve();
+  if (deps && deps.length > 0) {
+    document.getElementsByTagName("link");
+    const cspNonceMeta = document.querySelector(
+      "meta[property=csp-nonce]"
+    );
+    const cspNonce = (cspNonceMeta == null ? void 0 : cspNonceMeta.nonce) || (cspNonceMeta == null ? void 0 : cspNonceMeta.getAttribute("nonce"));
+    promise = Promise.allSettled(
+      deps.map((dep) => {
+        dep = assetsURL(dep);
+        if (dep in seen) return;
+        seen[dep] = true;
+        const isCss = dep.endsWith(".css");
+        const cssSelector = isCss ? '[rel="stylesheet"]' : "";
+        if (document.querySelector(`link[href="${dep}"]${cssSelector}`)) {
+          return;
+        }
+        const link = document.createElement("link");
+        link.rel = isCss ? "stylesheet" : scriptRel;
+        if (!isCss) {
+          link.as = "script";
+        }
+        link.crossOrigin = "";
+        link.href = dep;
+        if (cspNonce) {
+          link.setAttribute("nonce", cspNonce);
+        }
+        document.head.appendChild(link);
+        if (isCss) {
+          return new Promise((res, rej) => {
+            link.addEventListener("load", res);
+            link.addEventListener(
+              "error",
+              () => rej(new Error(`Unable to preload CSS for ${dep}`))
+            );
+          });
+        }
+      })
+    );
+  }
+  function handlePreloadError(err) {
+    const e = new Event("vite:preloadError", {
+      cancelable: true
+    });
+    e.payload = err;
+    window.dispatchEvent(e);
+    if (!e.defaultPrevented) {
+      throw err;
+    }
+  }
+  return promise.then((res) => {
+    for (const item of res || []) {
+      if (item.status !== "rejected") continue;
+      handlePreloadError(item.reason);
+    }
+    return baseModule().catch(handlePreloadError);
+  });
+};
 var dist = {};
 var hasRequiredDist;
 function requireDist() {
@@ -9541,12 +9605,14 @@ function App() {
   const mdFiles = ["/weekly/2025-01-12-Anne.md", "/weekly/2025-01-19-Anne.md"];
   reactExports.useEffect(() => {
     const fetchPosts = async () => {
+      const postFiles = /* @__PURE__ */ Object.assign({ "/README.md": () => __vitePreload(() => import("./README-i3lNGqXp.js"), true ? [] : void 0), "/public/templates/weekly1.md": () => __vitePreload(() => import("./weekly1-CbHS6a57.js"), true ? [] : void 0), "/public/weekly/2025-01-12-Anne.md": () => __vitePreload(() => import("./2025-01-12-Anne-CS7prWtz.js"), true ? [] : void 0), "/public/weekly/2025-01-19-Anne.md": () => __vitePreload(() => import("./2025-01-19-Anne-BqO3T_Z5.js"), true ? [] : void 0), "/src/weekly/2025-01-12-Anne.md": () => __vitePreload(() => import("./2025-01-12-Anne-u78yy-I3.js"), true ? [] : void 0), "/src/weekly/2025-01-19-Anne.md": () => __vitePreload(() => import("./2025-01-19-Anne-BadzgLM-.js"), true ? [] : void 0) });
+      console.log("postfiles", postFiles);
       const postPromises = mdFiles.map(async (filePath) => {
         console.log("Patttthhh", filePath);
         const date = dayjs(
-          filePath.split("/")[3].split("-").slice(0, 3).join("-")
+          filePath.split("/")[2].split("-").slice(0, 3).join("-")
         );
-        const player = filePath.split("/")[3].split("-").slice(-1);
+        const player = filePath.split("/")[2].split("-").slice(-1);
         return { filePath, date, player };
       });
       const posts2 = await Promise.all(postPromises);
---
+++ assets/weekly1-CbHS6a57.js
@@ -0,0 +1,4 @@
+const weekly1 = "data:text/markdown;base64,IyMjIPCfjq8gV2hhdCBkaWQgSSBEbz8KCiMjIyDwn4+EIFdoYXQgd2VudCBXRUxMPwoKIyMjIPCfpKggV2hhdCBjb3VsZCBoYXZlIGdvbmUgQkVUVEVSPwoKIyMjIPCfp5AgV2hhdCBkaWQgSSBMRUFSTj8K";
+export {
+  weekly1 as default
+};
